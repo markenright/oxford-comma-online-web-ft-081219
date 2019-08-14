7da72ea4c0fc40fb2878a01fd9924aa75f555652
Mark Enright <enright.mark@gmail.com>
2019-08-14 19:16:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
   if array.length == 1
     
     return array.join
-  end
+  
   
  elsif array.length == 2
   return array.join(" and ")
- end
+ 
  
  else
    last_one = array.pop
