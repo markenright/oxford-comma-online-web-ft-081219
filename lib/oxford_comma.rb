896b90198ef23d44cf8a25a0db4a529136deb280
Mark Enright <enright.mark@gmail.com>
2019-08-14 19:17:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    
+    return array.join
+  
+  
+ elsif array.length == 2
+  return array.join(" and ")
+ 
+ 
+ else
+   last_one = array.pop
+  array.join(", ") + ", and " + last_one
 end
+end
+
+puts oxford_comma(["fiddleheads","okra","kohlrabi"])
+puts
+puts oxford_comma(["something", "else"])
